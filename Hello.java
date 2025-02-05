2ed8f7a4a9a75b4b6f1298ecfa61f1b875519aa9
Jesus Orozco <jesusoro@umich.edu>
2025-02-05 17:02:04 -0500

    removed for loop

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        for(int i = 1; i <= 20; i++) {
-            // useless comment here
-            System.out.println("Hello World..." + i);
-        } // for
+            System.out.println("Hello World");
     } // main
 }
